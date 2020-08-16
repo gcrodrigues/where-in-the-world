f8567eee4c88ce3ffd4518ec83c265282a6b4fcc
Gustavo <gcrodrigues413@gmail.com>
2020-08-15 21:56:51 -0300

    themes file

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "dark",
+
+  colors: {
+    background: "hsl(207, 26%, 17%)",
+    complement: "hsl(209, 23%, 22%)",
+    input: "hsl(0, 0%, 100%)",
+    text: "hsl(0, 0%, 100%)",
+    switchOn: "#FF8C00",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "light",
+
+  colors: {
+    background: "hsl(0, 0%, 98%)",
+    complement: "hsl(0, 0%, 100%)",
+    input: "hsl(0, 0%, 52%)",
+    text: "hsl(200, 15%, 8%)",
+    switchOn: "#FF8C00",
+  },
+};
