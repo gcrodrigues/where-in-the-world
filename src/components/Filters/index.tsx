090cb412b3f25c2e94383d4a64497dc078677e81
Gustavo <gcrodrigues413@gmail.com>
2020-08-16 20:21:56 -0300

    Search by name and region methods

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,28 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, ChangeEvent } from "react";
 import { MdSearch } from "react-icons/md";
 import { Container, InputWrapper, SelectWrapper } from "./styles";
 import { ThemeContext } from "styled-components";
+import ApiContext from "../../Context/ApiContext";
 
 const Filters: React.FC = () => {
   const { colors } = useContext(ThemeContext);
+  const {
+    searchCountry,
+    setSearchCountry,
+    setSelectedRegion,
+    selectedRegion,
+  } = useContext(ApiContext);
   return (
     <Container>
       <InputWrapper>
         <MdSearch color={colors.input} />
         <input
+          onChange={(e: ChangeEvent<HTMLInputElement>): void =>
+            setSearchCountry(e.target.value)
+          }
+          value={searchCountry}
           type="text"
           name="country"
           placeholder="Search for a country..."
         />
       </InputWrapper>
       <SelectWrapper>
-        <select name="" id="">
-          <option>Filter by Region</option>
-          <option>Africa</option>
-          <option>Americas</option>
-          <option>Asia</option>
-          <option>Europe</option>
-          <option>Oceania</option>
+        <select
+          onChange={(e: ChangeEvent<HTMLSelectElement>): void =>
+            setSelectedRegion(e.target.value)
+          }
+          value={selectedRegion}
+          name="region"
+          id="region"
+        >
+          <option value="0">Filter by Region</option>
+          <option value="africa">Africa</option>
+          <option value="americas">Americas</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
         </select>
       </SelectWrapper>
     </Container>
--- src/components/FlagList/index.tsx
+++ src/components/FlagList/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import FlagCard from "../../components/FlagCard";
 import { Container } from "./styles";
 import api from "../../services/api";
+import ApiContext from "../../Context/ApiContext";
 
 interface Flag {
   name: string;
@@ -13,6 +14,7 @@ interface Flag {
 
 const FlagList: React.FC = () => {
   const [flags, setFlags] = useState<Flag[]>([]);
+  const { selectedRegion, searchCountry } = useContext(ApiContext);
 
   useEffect(() => {
     async function handleFlagsData() {
@@ -27,10 +29,45 @@ const FlagList: React.FC = () => {
     handleFlagsData();
   }, []);
 
+  useEffect(() => {
+    async function handleRegionCountries() {
+      try {
+        if (selectedRegion === "0") {
+          const response = await api.get("all");
+          setFlags(response.data);
+        } else {
+          const response = await api.get(`region/${selectedRegion}`);
+          setFlags(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    handleRegionCountries();
+  }, [selectedRegion]);
+
+  useEffect(() => {
+    async function handleSearchCountriesByName() {
+      try {
+        if (searchCountry === "") {
+          return;
+        }
+        const response = await api.get(`name/${searchCountry}`);
+        setFlags(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    handleSearchCountriesByName();
+  }, [searchCountry]);
+
   return (
     <Container>
       {flags.map((flag) => (
         <FlagCard
+          key={flag.name}
           image={flag.flag}
           country={flag.name}
           population={flag.population}
